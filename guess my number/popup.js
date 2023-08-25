5634e2f226b3b6f023bdb945d653f88b57da5560
Eliezer Hembrom <99393844+Eliezerhembrom@users.noreply.github.com>
2023-08-25 17:38:43 +0530

    Added guess my number files

---
+++ guess my number/popup.js
@@ -0,0 +1,3 @@
+import party from 'party-js'
+
+const party = require('party-js');
--- guess my number/script.js
+++ guess my number/script.js
@@ -1,32 +1,69 @@
 'use strict'
-// console.log(document.querySelector('.score').textContent);
 
-// console.log(document.querySelector('.input-btn').value);
-const randomNumber = Math.floor(Math.random()* 20);
-
+const guessList = document.getElementById("myList");
+const scoreList = document.getElementById('scoreList')
+const randomNumber = Math.floor(Math.random()* 20) + 1;
+const myList = document.getElementById('myList');
+let score = 20;
+let highscore = 0;
+let lastScore = 0;
 document.querySelector('.check').addEventListener('click', function(){
   const inputNumber = document.querySelector('.input-btn').value;
   const guess = Number(document.querySelector('.input-btn').value);
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = 'blue';
+  if(guess){
+    score--;
+  }
+  document.querySelector('.score').textContent = score;
+
+
   if(!guess){
     document.querySelector('.guessing').textContent = 'Please Enter a Number';
-  }else if(guess === 10){
+  }else if(guess === randomNumber){
+    lastScore = score;
     document.querySelector('.guessing').textContent = 'Correct Number!';
     document.querySelector('.number').textContent = inputNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-  }else if(guess < 10){
-    document.querySelector('.guessing').textContent = 'Too Low!';
+    document.querySelector('body').style.backgroundColor = '#202A6F';
     document.querySelector('.input-btn').value = '';
+    const currentDate = new Date();
+    const hours = currentDate.getHours().toString().padStart(2, '0'); 
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0'); 
+    
+    const formattedTime = `${hours}:${minutes}:${seconds}`;  
+      const newGuessListItem = document.createElement("li");
+      newGuessListItem.textContent = `${formattedTime}`;
+      guessList.appendChild(newGuessListItem);
+  
+      const newScoreListItem = document.createElement("li");
+      newScoreListItem.textContent = `${score}`;
+      scoreList.appendChild(newScoreListItem);
+
+      party.confetti(this, {
+          count: party.variation.range(20, 40),
+      });
+
+
+  }else if(guess < randomNumber){
+    document.querySelector('.guessing').textContent = 'Try Higher!';
+    document.querySelector('.input-btn').value = '';
+
   }else{
-    document.querySelector('.guessing').textContent = 'Too High!';
+    document.querySelector('.guessing').textContent = 'Try Lower!';
     document.querySelector('.input-btn').value = '';
   }
+
 })
 
+
 document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
   document.querySelector('.input-btn').value = '';
   document.querySelector('.number').textContent = '?';
-  document.querySelector('body').style.backgroundColor = 'blue';
+  document.querySelector('body').style.backgroundColor = '#7054bf';
   document.querySelector('.guessing').textContent = 'Start guessing...';
+  document.querySelector('.highscore').textContent = lastScore;
+  document.querySelector('.score').textContent = score;
+
 });
